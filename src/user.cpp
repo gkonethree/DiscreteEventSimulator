a5db482e13b6cba8db891ceec0b5c649569c63cf
KV1354 <kaushalvijayvergiya1234@gmail.com>
2026-03-19 21:57:55 +0530

    Added randomness in service time

--- src/user.cpp
+++ src/user.cpp
@@ -6,7 +6,10 @@ std::mt19937 gen;
 
 
 ListOfEvents User::receive(std::shared_ptr<RequestUnloadEvent> event) {
-    Time thinkTime = std::max((Time)0, (Time)this->thinkTime(gen));
+
+    Time serviceTime = generateServiceTime();
+    Time thinkTime = generateThinkTime();
+
     Time responseTime=event->time + thinkTime - event->request->startTime;
     if( responseTime>= timeout){
         metrics->insertBadEvent(responseTime);
@@ -16,14 +19,49 @@ ListOfEvents User::receive(std::shared_ptr<RequestUnloadEvent> event) {
     }
     Time rSysTime = event->time - event->request->startTime;
     metrics->insertRsysEvent(rSysTime);
-
-    ListOfEvents consequences;
     
+    ListOfEvents consequences;
+
     std::shared_ptr<Request> request = std::make_shared<Request>(
-        requestProcessingTime, *this, event->time + thinkTime
+        serviceTime, *this, event->time + thinkTime
     );
     std::shared_ptr<Event> new_event = std::make_shared<RequestLoadEvent>(
         event->time +thinkTime, *(this->serverLink), request
+    );
+    consequences.push_back(new_event);
+    return std::move(consequences);
+}
+
+Time User::generateServiceTime(){
+    Time serviceTime;
+    if (distribution == Distribution::Constant){
+        serviceTime = constantServiceTime;
+    }
+    else if (distribution == Distribution::Exponential){
+        serviceTime = std::max((Time) 0, (Time) expServiceTime(gen));
+    }
+    else{
+        serviceTime = std::max((Time) 0, uniformServiceTime(gen));
+    }
+    return serviceTime;
+}
+
+
+Time User::generateThinkTime(){
+    return std::max((Time)0, (Time)this->thinkTime(gen));
+}
+
+ListOfEvents User::init(Time time) {
+
+    ListOfEvents consequences;
+    Time serviceTime = generateServiceTime();
+    Time thinkTime = generateThinkTime();
+
+    std::shared_ptr<Request> request = std::make_shared<Request>(
+        serviceTime, *this, time
+    );
+    std::shared_ptr<Event> new_event = std::make_shared<RequestLoadEvent>(
+        time +thinkTime, *(this->serverLink), request
     );
     consequences.push_back(new_event);
     return std::move(consequences);
